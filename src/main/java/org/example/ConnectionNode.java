01d55e03a7ce3cb29c73ca1f06f4d93bf5b95de8
Hugo <1621740564@qq.com>
2023-09-16 11:06:50 +0800

    established a simple demo

---
+++ src/main/java/org/example/ConnectionNode.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2021, 2023, THL A29 Limited, a Tencent company. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class ConnectionNode {
+    private static String url = "jdbc:mysql://localhost:3306/test?useSSL=false";
+    private static String u = "root";
+    private static String p = "root";
+
+    ConnectionNode next = null;
+    Connection con;
+    Statement stm;
+
+    public ConnectionNode() {
+        try {
+            con = DriverManager.getConnection(url, u, p);
+            stm = con.createStatement();
+        } catch (Exception e) {
+        }
+    }
+}
